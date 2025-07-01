6c59aa31822ce01c604157df0e57703883feff01
anharsaja <anharmuk12@gmail.com>
2025-07-01 13:29:20 +0700

    :sparkles: feat: layout & components vue

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>Admin Dashboard</div>
+
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+
+        <!-- navbar start -->
+        <Navbar/>        
+        <!-- end -->
+
+        <!-- Sidebar -->
+        <Sidebar/>
+
+        <!-- sidebar start -->
+
+        <!-- end -->
+
+        <slot/>
+    </div>
+</template>
+
+
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+import Navbar from './Navbar.vue'
+import Sidebar from './Sidebar.vue'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+});
+</script>
