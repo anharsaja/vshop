216300c6640ee0dc59fc648c764ec0b7961a6283
anharsaja <anharmuk12@gmail.com>
2025-07-01 11:04:59 +0700

    :tada: initial project

---
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class AdminAuthController extends Controller
+{
+    public function showLoginForm() 
+    {
+        return Inertia::render('Admin/Auth/Login');
+    }
+
+    public function login(Request $request)
+    {
+        // logic login here
+        // check if the user is an admin and redirect accordingly
+        if (Auth::attemot(['email' => $request->email, 'password' => $request->password, 'isAdmin' => true]))
+        {
+            return redirect()->route('admin.dashboard'); //redirect to the admin dashboard
+        }
+
+        return redirect()->route('admin.login')->with('error', 'Invalid credentials.');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+
+        return redirect()->route('admin.login');
+    }
+}
