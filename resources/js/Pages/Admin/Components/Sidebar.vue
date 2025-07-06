6c47163719df00cf2d9153ee0f436993b2e75e8f
anharsaja <anharmuk12@gmail.com>
2025-07-07 00:27:05 +0700

    :bug: fix: Temp rename to reset casing

--- resources/js/Pages/Admin/Components/Sidebar.vue
+++ resources/js/Pages/Admin/Components/Sidebar.vue
@@ -21,7 +21,7 @@
 			</form>
 			<ul class="space-y-2">
 				<li>
-					<a href="#"
+					<Link :href="route('admin.dashboard')"
 						class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
 						<svg aria-hidden="true"
 							class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -30,7 +30,7 @@
 							<path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
 						</svg>
 						<span class="ml-3">Dashboard</span>
-					</a>
+					</Link>
 				</li>
 				<li>
 					<button type="button"
@@ -53,8 +53,8 @@
 					</button>
 					<ul id="dropdown-pages" class="hidden py-2 space-y-2">
 						<li>
-							<a href="#"
-								class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Product</a>
+							<Link :href="route('admin.products.index')"
+								class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Product</Link>
 						</li>
 						<li>
 							<a href="#"
@@ -234,3 +234,7 @@
 		</div>
 	</aside>
 </template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
