a3e6fb5f32510ad0186858bb1b12975c2cdc67e0
anharsaja <anharmuk12@gmail.com>
2025-07-30 00:46:54 +0700

    :sparkles: feat: Setup Authenticated User Header

--- resources/js/Pages/User/Layout/Navbar.vue
+++ resources/js/Pages/User/Layout/Navbar.vue
@@ -1,24 +1,77 @@
+<script setup>
+import { usePage, Link } from '@inertiajs/vue3';
+
+const canLogin = usePage().props.canLogin;
+const canRegister = usePage().props.canRegister;
+const auth = usePage().props.auth;
+</script>
+
 <template>
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
-            <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button"
-                    class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            <!-- logo  -->
+            <Link :href="route('admin.dashboard')" class="flex items-center justify-between mr-4">
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <path fill-rule="evenodd"
+                    d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z"
+                    clip-rule="evenodd" />
+            </svg> -->
+            <img src="https://img.icons8.com/?size=100&id=fdWDYJjuHBaN&format=png&color=000000" class="size-11" alt="">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                V.shop
+            </span>
+            </Link>
+
+            <!-- dropdown -->
+            <div v-if="canLogin" class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <div class="pr-2">
+                    <button type="button"
+                        class="flex text-sm bg-gray-300 rounded-full md:me-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-100">
+                        <img class="w-8 h-8 rounded-full"
+                            src="https://img.icons8.com/?size=100&id=qKC6rrfXuwKA&format=png&color=000000"
+                            alt="user photo">
+                    </button>
+                </div>
+                <button v-if="auth.user" type="button"
+                    class="flex text-sm bg-gray-300 rounded-full md:me-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-100"
                     id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
                     data-dropdown-placement="bottom">
                     <span class="sr-only">Open user menu</span>
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo">
+                    <img class="w-8 h-8 rounded-full"
+                        src="https://img.icons8.com/?size=100&id=PzB12GFxfCfO&format=png&color=000000" alt="user photo">
                 </button>
+
+
+                <div v-else>
+                    <Link type="button" :href="route('login')"
+                        class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                    Login</Link>
+
+                    <Link :href="route('register')" v-if="canRegister"
+                        class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                    <span
+                        class="relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                        Register
+                    </span>
+                    </Link>
+                </div>
+
+                <div class="pl-2">
+                    <button type="button"
+                        class="flex text-sm bg-gray-300 rounded-full md:me-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-100">
+                        <img class="w-8 h-8 rounded-full"
+                            src="https://img.icons8.com/?size=100&id=s3wv9JoNzsta&format=png&color=000000"
+                            alt="user photo">
+                    </button>
+                </div>
+
                 <!-- Dropdown menu -->
                 <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600"
                     id="user-dropdown">
                     <div class="px-4 py-3">
                         <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                        <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                        <span
+                            class="block text-sm  text-gray-500 truncate dark:text-gray-400">$/{auth.user.email}</span>
                     </div>
                     <ul class="py-2" aria-labelledby="user-menu-button">
                         <li>
@@ -34,9 +87,10 @@
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
                         </li>
                         <li>
-                            <a href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                                out</a>
+                            <Link :href="route('logout')" method="post" as="button"
+                                class="block w-full text-left py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                            Sign out
+                            </Link>
                         </li>
                     </ul>
                 </div>
@@ -51,6 +105,8 @@
                     </svg>
                 </button>
             </div>
+            <!-- end dropdown -->
+
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
                 <ul
                     class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
