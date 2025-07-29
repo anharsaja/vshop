5ee7f77a5ae216676ee62438b7df8db6eae5047b
anharsaja <anharmuk12@gmail.com>
2025-07-30 00:13:49 +0700

    :bug: fix: UI dropdown

--- resources/js/Pages/Admin/Components/Navbar.vue
+++ resources/js/Pages/Admin/Components/Navbar.vue
@@ -478,7 +478,7 @@
                     <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
                         <li>
                             <Link :href="route('logout')" method="post" as="button"
-                                class="block w-full py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                class="block w-full text-left py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">
                             Sign out
                             </Link>
                         </li>
